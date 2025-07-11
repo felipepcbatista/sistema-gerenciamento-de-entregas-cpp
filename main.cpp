968e9c7da41296bec6782d8112361c3a3806cae8
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-07-11 10:29:11 -0300

    Estruturação do código-fonte

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <limits>
 
+class Local{
+//Local vai aqui
+};
+
+class Veiculo{
+//Veículo vai aqui
+};
+
+class Pedido{
+//Pedido vai aqui
+};
+
+class Gerenciador{
+//Gerenciador de Locais, Veículos e Pedidos vai aqui
+};
+
+class Rotas{
+//Rotas vai aqui
+};
+
+int main()
+{
+    //Main vai aqui
+}
