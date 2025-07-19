c170c3da23de11d5fb06f7b114509adf26fb4d1c
Felipe Paz Carvalho Batista <lipepaz23@gmail.com>
2025-07-18 21:29:49 -0300

    ManagerLocais.h

---
+++ include/ManagerLocais.h
@@ -0,0 +1,22 @@
+#ifndef MANAGERLOCAIS_H
+#define MANAGERLOCAIS_H
+
+class ManagerLocais{
+private:
+    Local locais[MAX];
+    int totalLocais=0;
+public:
+    void cadastrarLocal();
+    void atualizarLocal();
+    void listaLocais();
+    void removerLocal();
+    const char* getEnderecoByID(int);
+    int getIDporEndereco(const char*);
+    void setTotalLocais(int);
+    double getCoordenadaXporID(int)const;
+    double getCoordenadaYporID(int)const;
+    int getTotalLocais()const;
+    Local* getArrayLocais()const
+};
+
+#endif
